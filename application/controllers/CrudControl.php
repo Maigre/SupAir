44d24f6b5f7f2bcd27e71ea8790e80743355a200
Maigre <thomas.bohl@gmail.com>
2012-04-12 15:24:24 +0200

    MGR CrudControl save and delete with rooted json

--- application/controllers/CrudControl.php
+++ application/controllers/CrudControl.php
@@ -42,43 +42,28 @@ class CrudControl extends CI_Controller {
 	{
 		//posted result array
 		$results = $this->input->post();
-		 
-		//if id posted or passed to the controller, use it (switch to edit mode)
-		if ((!$id) && (isset($results['id']))) $id = $results['id'];
 		
-		//set and save results		
-		jse($this->Entity($id)->set($results)->save());
-	}
-	
-	function savewithroot($id=false)
-	{
-		//posted result array
-		$results = $this->input->post();
-		 
 		//Cas où le post est encodé en json et nécessite une root (='data')
-		if(isset($results['data'])){
+		if(isset($results['data']))
+		{
+			$answer_type = 'rooted';
 			$results = $results['data'];
 			$results = json_decode( $results );
 			$results = get_object_vars ( $results ); //transforme l'objet en tableau
 		}
-		
+		 
 		//if id posted or passed to the controller, use it (switch to edit mode)
 		if ((!$id) && (isset($results['id']))) $id = $results['id'];
 		
-		//set and save results		
-		$response=$this->Entity($id)->set($results)->save();
-		//if ($response['success']){
-			
-		foreach($results as $key=>$value){
-			if ($key!='id'){
-				$response[$key]=$value;
-			}				
-		}
-		$response=array(
-			'data'=>$response
-		);
+		//set and save results
+		$save = $this->Entity($id)->set($results)->save();
 		
-		jse($response);
+		//set up answer
+		$save_res = null;
+		if ($answer_type == 'rooted') $save_res['data'] = $save;
+		else $save_res = $save;
+		 		
+		jse($save);
 	}
 	
 	function delete($id=false)
@@ -86,20 +71,10 @@ class CrudControl extends CI_Controller {
 		//posted result array
 		$results = $this->input->post(); 
 		
-		//if id posted or passed to the controller, use it (switch to edit mode)
-		if ((!$id) && ($results['id'] > 0)) $id = $results['id'];
-		
-		//delete!	
-		jse($this->Entity($id)->delete());
-	}
-	
-	function deletewithroot($id=false)
-	{
-		//posted result array
-		$results = $this->input->post(); 
-		
 		//Cas où le post est encodé en json et nécessite une root (='data')
-		if(isset($results['data'])){
+		if(isset($results['data']))
+		{
+			$answer_type = 'rooted';
 			$results = $results['data'];
 			$results = json_decode( $results );
 			$results = get_object_vars ( $results ); //transforme l'objet en tableau
@@ -109,18 +84,14 @@ class CrudControl extends CI_Controller {
 		if ((!$id) && ($results['id'] > 0)) $id = $results['id'];
 		
 		//delete!	
-		$response = $this->Entity($id)->delete();
+		$del = $this->Entity($id)->delete();
 		
-		foreach($results as $key=>$value){
-			if ($key!='id'){
-				$response[$key]=$value;
-			}				
-		}
-		$response=array(
-			'data'=>$response
-		);
+		//set up answer
+		$del_res = null;
+		if ($answer_type == 'rooted') $del_res['data'] = $del;
+		else $del_res = $del;
 		
-		jse($response);
+		jse($del_res);
 	}
 	
 	function listAll($order=false)
@@ -132,18 +103,17 @@ class CrudControl extends CI_Controller {
 		if ($posted = $this->input->post())
 		foreach ($posted as $id=>$val)
 		{
-			//echo $id;
 			if (($id == 'where') and (is_array($val))) foreach ($val as $w=>$v) $post['where'][$w] = $v;
 			else
 			{
+				//old method to specify WHERE
 				$where_id = explode('__',$id);
 				if (isset($where_id[1]) and ($where_id[1])) $post['where'][$where_id[1]] = $val;
 			}
 		}
 				
-		if (isset($post['where']) and (is_array($post['where']))){
-			//echo $post['where'];
-			//echo  json_decode($post['where']);
+		if (isset($post['where']) and (is_array($post['where'])))
+		{
 			foreach($post['where'] as $name=>$value) $this->db->where($name,$value);
 		}
 				
